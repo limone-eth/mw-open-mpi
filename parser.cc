47e801e3f683af8ac6e6fa54f88d9f8756944fac
FrancescoAmorosini <francesco.amorosini@stud.unifi.it>
2020-04-13 15:32:14 +0200

    csv parser

---
+++ parser.cc
@@ -0,0 +1,39 @@
+#include <iostream>     // cout, endl
+#include <fstream>      // fstream
+#include <vector>
+#include <string>
+#include <algorithm>    // copy
+#include <iterator>     // ostream_operator
+#include <boost/tokenizer.hpp>
+
+int main()
+{
+    using namespace std;
+    using namespace boost;
+    string data("./sample.csv");
+
+    ifstream in(data.c_str());
+    if (!in.is_open()) return 1;
+
+    typedef tokenizer< escaped_list_separator<char> > Tokenizer;
+    vector< vector<string> > table;
+    vector<string> headers;
+    string line;
+
+    while (getline(in,line))
+    {   
+        vector <string> vec;
+        Tokenizer tok(line);
+        vec.assign(tok.begin(),tok.end());
+
+        // vector now contains strings from one row, output to cout here
+        copy(vec.begin(), vec.end(), ostream_iterator<string>(cout, "|"));
+
+        cout << "\n----------------------" << endl;
+        if(headers.empty())
+            headers = vec;
+        else
+            table.push_back(vec);
+    }
+    in.close();
+}

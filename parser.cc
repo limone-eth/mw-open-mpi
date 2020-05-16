0fab26293ebfbdf5b762d4abf0a2226dcdb355af
FrancescoAmorosini <francesco.amorosini@stud.unifi.it>
2020-05-16 14:29:02 +0200

    week type is now string + fixed Q1

--- parser.cc
+++ parser.cc
@@ -21,7 +21,7 @@ int colIndex(string col) {
     return find(headers.begin(), headers.end(), col) - headers.begin();
 }
 
-ostream &operator<<(ostream &os, const std::pair<std::string, int> &p) {
+ostream &operator<<(ostream &os, const std::pair<std::string, std::string> &p) {
     os << p.first << ' ' << p.second << ' ';
     return os;
 }
@@ -39,14 +39,14 @@ public:
 
     int total_kills;
     int total_accidents;
-    int week_of_year;
+    string week_of_year;
 } car_accident;
 
 class Query1 {
 public:
-    int year;
+    std::string year;
     int total_accidents;
-    int week;
+    std::string week;
 };
 
 class Query2 {
@@ -59,7 +59,7 @@ public:
 class Query3 {
 public:
     std::string borough;
-    int week;
+    std::string week;
     int total_accidents;
     int lethal_accidents;
     float lethal_average;
@@ -70,20 +70,21 @@ bool in_array(const std::string &value, const std::vector<string> &array) {
     return std::find(array.begin(), array.end(), value) != array.end();
 }
 
-void evaluateQuery1(vector<CarAccident> car_accidents) {
-    std::map<int, int> query_results_1;
+void evaluateQueries(vector<CarAccident> car_accidents) {
+    std::map<std::string, Query1> query_results_1;
     std::map<std::string, Query2> query_results_2;
-    std::map<std::pair<std::string, int>, Query3> query_results_3;
+    std::map<std::pair<std::string, std::string>, Query3> query_results_3;
     omp_set_num_threads(NUM_THREADS);
     int chunkSize = car_accidents.size() / omp_get_max_threads();
 #pragma omp for schedule(dynamic, chunkSize)
     for (long unsigned int i = 0; i < car_accidents.size(); i++) {
         // QUERY 1
-        if (query_results_1.count(car_accidents[i].week_of_year) == false) {
-            query_results_1[car_accidents[i].week_of_year] = car_accidents[i].total_kills;
+        std::string s = car_accidents[i].date.substr(6)+ " W:" + car_accidents[i].week_of_year;
+        if (query_results_1.count(s) == false) {
+            query_results_1[s].total_accidents = car_accidents[i].total_kills;
         } else {
 #pragma omp atomic
-            query_results_1[car_accidents[i].week_of_year] += car_accidents[i].total_kills;
+            query_results_1[s].total_accidents += car_accidents[i].total_kills;
         }
 
         // QUERY 2
@@ -199,9 +200,9 @@ void evaluateQuery1(vector<CarAccident> car_accidents) {
         }
 
         // QUERY 3
-        std::vector<std::pair<std::string, int>> boroughs_weeks;
+        std::vector<std::pair<std::string, std::string>> boroughs_weeks;
         if (car_accidents[i].borough != "") {
-            std::pair<std::string, int> b_w = std::make_pair(car_accidents[i].borough,
+            std::pair<std::string, std::string> b_w = std::make_pair(car_accidents[i].borough,
                                                              car_accidents[i].week_of_year);
             boroughs_weeks.push_back(b_w);
             if (query_results_3.count(b_w) == false) {
@@ -227,7 +228,7 @@ void evaluateQuery1(vector<CarAccident> car_accidents) {
 
     cout << "----- QUERY 1 -----" << endl;
     for (const auto&[k, v] : query_results_1)
-        std::cout << "week[" << k << "] = total_lethal_accidents(" << v << ") " << std::endl;
+        std::cout << "week[" << k << "] = total_lethal_accidents(" << v.total_accidents << ") " << std::endl;
     cout << "-------------------" << endl;
 
     cout << "----- QUERY 2 -----" << endl;
@@ -243,161 +244,7 @@ void evaluateQuery1(vector<CarAccident> car_accidents) {
     cout << "-------------------" << endl;
 }
 
-/*std::map<string, Query2> evaluateQuery2(vector<CarAccident> car_accidents) {
-    std::map<string, Query2> query_results;
-    omp_set_num_threads(NUM_THREADS);
-    int chunkSize = car_accidents.size()/omp_get_max_threads();
-    #pragma omp for schedule(dynamic, chunkSize)
-    for (long unsigned int i = 0; i < car_accidents.size(); i++) {
-        vector<string> already_processed_factors;
-        // CHECK FACTOR 1
-        if (car_accidents[i].factor_1 != ""){
-            already_processed_factors.push_back(car_accidents[i].factor_1);
-            if (query_results.count(car_accidents[i].factor_1) == false) {
-                query_results[car_accidents[i].factor_1].total_accidents = 1;
-                query_results[car_accidents[i].factor_1].lethal_accidents = car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_1].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_1].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_1].total_accidents;
-            }else {
-                #pragma omp critical
-                {
-                query_results[car_accidents[i].factor_1].total_accidents += 1;
-                query_results[car_accidents[i].factor_1].lethal_accidents += car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_1].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_1].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_1].total_accidents;
-                }
-            }
-        }
-        // CHECK FACTOR 2
-        if (!in_array(car_accidents[i].factor_2, already_processed_factors) && car_accidents[i].factor_2 != "") {
-            already_processed_factors.push_back(car_accidents[i].factor_2);
-            if (query_results.count(car_accidents[i].factor_2) == false) {
-                query_results[car_accidents[i].factor_2].total_accidents = 1;
-                query_results[car_accidents[i].factor_2].lethal_accidents = car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_2].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_2].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_2].total_accidents;
-            }else {
-                #pragma omp critical
-                {
-                query_results[car_accidents[i].factor_2].total_accidents += 1;
-                query_results[car_accidents[i].factor_2].lethal_accidents += car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_2].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_2].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_2].total_accidents;
-                }
-            }
-        }
-        // CHECK FACTOR 3
-        if (!in_array(car_accidents[i].factor_3, already_processed_factors) && car_accidents[i].factor_3 != "") {
-            already_processed_factors.push_back(car_accidents[i].factor_3);
-            if (query_results.count(car_accidents[i].factor_3) == false) {
-                query_results[car_accidents[i].factor_3].total_accidents = 1;
-                query_results[car_accidents[i].factor_3].lethal_accidents = car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_3].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_3].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_3].total_accidents;
-            }else {
-                #pragma omp critical
-                {
-                query_results[car_accidents[i].factor_3].total_accidents += 1;
-                query_results[car_accidents[i].factor_3].lethal_accidents += car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_3].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_3].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_3].total_accidents;
-            }
-            }
-        }
-        // CHECK FACTOR 4
-        if (!in_array(car_accidents[i].factor_4, already_processed_factors) && car_accidents[i].factor_4 != "") {
-            already_processed_factors.push_back(car_accidents[i].factor_4);
-            if (query_results.count(car_accidents[i].factor_4) == false) {
-                query_results[car_accidents[i].factor_4].total_accidents = 1;
-                query_results[car_accidents[i].factor_4].lethal_accidents = car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_4].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_4].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_4].total_accidents;
-            }else {
-                #pragma omp critical
-                {
-                query_results[car_accidents[i].factor_4].total_accidents += 1;
-                query_results[car_accidents[i].factor_4].lethal_accidents += car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_4].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_4].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_4].total_accidents;
-            }
-            }
-        }
-        // CHECK FACTOR 5
-        if (!in_array(car_accidents[i].factor_5, already_processed_factors) && car_accidents[i].factor_5 != "") {
-            already_processed_factors.push_back(car_accidents[i].factor_5);
-            if (query_results.count(car_accidents[i].factor_5) == false) {
-                query_results[car_accidents[i].factor_5].total_accidents = 1;
-                query_results[car_accidents[i].factor_5].lethal_accidents = car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_5].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_5].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_5].total_accidents;
-            }else {
-                #pragma omp critical
-                {
-                query_results[car_accidents[i].factor_5].total_accidents += 1;
-                query_results[car_accidents[i].factor_5].lethal_accidents += car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[car_accidents[i].factor_5].lethal_percentage =
-                        (float)query_results[car_accidents[i].factor_5].lethal_accidents /
-                                (float)query_results[car_accidents[i].factor_5].total_accidents;
-            }
-            }
-        }
-
-    }
-    #pragma omp barrier
-    cout << "----- QUERY 2 -----" << endl;
-    for (const auto&[k, v] : query_results)
-        std::cout << "factor[" << k << "] = total_accidents(" << v.total_accidents << ") | lethal_accidents(" << v.lethal_accidents << ") | lethal_percentage(" << v.lethal_percentage << ") "<< std::endl;
-    cout << "-------------------" << endl;
-    return query_results;
-}
-
-std::map<std::pair<std::string, int>, Query3> evaluateQuery3(vector<CarAccident> car_accidents){
-    std::map<std::pair<std::string, int>, Query3> query_results;
-    omp_set_num_threads(NUM_THREADS);
-    int chunkSize = car_accidents.size()/omp_get_max_threads();
-    #pragma omp parallel for schedule(dynamic, chunkSize)
-    for (long unsigned int i = 0; i < car_accidents.size(); i++) {
-        std::vector<std::pair<std::string, int>> boroughs_weeks;
-        if (car_accidents[i].borough != ""){
-            std::pair<std::string, int> b_w = std::make_pair(car_accidents[i].borough, car_accidents[i].week_of_year);
-            boroughs_weeks.push_back(b_w);
-            if (query_results.count(b_w) == false) {
-                query_results[b_w].total_accidents = 1;
-                query_results[b_w].week = car_accidents[i].week_of_year;
-                query_results[b_w].lethal_accidents = car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[b_w].lethal_average =
-                        (float)query_results[b_w].lethal_accidents /
-                                (float)query_results[b_w].total_accidents;
-            }else {
-                #pragma omp critical
-                {
-                query_results[b_w].total_accidents += 1;
-                query_results[b_w].lethal_accidents += car_accidents[i].total_kills > 0 ? 1 : 0;
-                query_results[b_w].lethal_average =
-                        (float)query_results[b_w].lethal_accidents /
-                                (float)query_results[b_w].total_accidents;
-            }
-        }
-    }
-    }
-    #pragma omp barrier
-    cout << "----- QUERY 3 -----" << endl;
-    for (const auto&[k, v] : query_results)
-        std::cout << "Borough[" << k << "] = total_accidents(" << v.total_accidents << ") | lethal_accidents(" << v.lethal_accidents << ") | average_lethal(" << v.lethal_average << ") "<< std::endl;
-    cout << "-------------------" << endl;
-    return query_results;
-}*/
-
-int get_week(std::string date) {
+std::string get_week(std::string date) {
     std::string formatted_date;
     std::string delimiter = "/";
     size_t pos = 0;
@@ -413,7 +260,7 @@ int get_week(std::string date) {
     short unsigned int month = std::atoi(split[0].c_str());
     short unsigned int day = std::atoi(split[1].c_str());
     boost::gregorian::date d{year, month, day};
-    return d.week_number();
+    return to_string(d.week_number());
 
 }
 
@@ -469,7 +316,7 @@ int main() {
 
 #pragma omp barrier
     auto start_1 = high_resolution_clock::now();
-    evaluateQuery1(car_accidents);
+    evaluateQueries(car_accidents);
     auto stop_1 = high_resolution_clock::now();
     auto duration_1 = duration_cast<microseconds>(stop_1 - start_1);
     cout << "DURATION FILE - " << duration_file.count() << endl;

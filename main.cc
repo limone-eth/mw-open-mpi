14fe0a2988543635ec8efa2ce031a8a391a2f850
FrancescoAmorosini <francesco.amorosini@stud.unifi.it>
2020-06-27 15:35:15 +0200

    removed rank

--- main.cc
+++ main.cc
@@ -205,7 +205,7 @@ int main() {
     MPI_Reduce(&local_lethal_accidents_per_week[0], &global_lethal_accidents_per_week[0], WEEKS, MPI_INT, MPI_SUM, 0,
                MPI_COMM_WORLD);
 
-    if (PROCESS_RANK == 0 && PRINT_RESULTS == true) {
+    if (PRINT_RESULTS == true) {
         cout << "QUERY 1 completed -> " << MPI_Wtime() << endl;
 
         cout << "LETHAL ACCIDENTS PER WEEK" << endl;
@@ -309,7 +309,7 @@ int main() {
     MPI_Reduce(&local_lethal_accidents_per_factor[0], &global_lethal_accidents_per_factor[0], global_factors.size(),
                MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    if (PROCESS_RANK == 0 && PRINT_RESULTS == true) {
+    if ( PRINT_RESULTS == true) {
         cout << "QUERY 2 completed -> " << MPI_Wtime() << endl;
 
         cout << "ACCIDENTS AND PERCENTAGE OF L/NL ACCIDENTS PER CONTRIBUTING FACTOR" << endl;
@@ -419,7 +419,7 @@ int main() {
         MPI_Reduce(&local_accidents_per_borough_per_week[b.second][0],
                    &global_accidents_per_borough_per_week[b.second][0], WEEKS, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    if (PROCESS_RANK == 0 && PRINT_RESULTS == true) {
+    if (PRINT_RESULTS == true) {
         cout << "QUERY 3 completed -> " << MPI_Wtime() << endl;
 
         for (const auto &b: global_boroughs) {
